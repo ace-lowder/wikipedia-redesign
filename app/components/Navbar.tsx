d60112b7f48e6e45db74ab29e669f727f18850d7
Ace <ace.lowder@gmail.com>
2024-09-05 10:31:38 -0700

    Install react icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,12 @@
+import { HiOutlineMenu } from "react-icons/hi";
+
 const Navbar = () => {
-  return <nav className="w-full p-4 flex gap-4">Navbar</nav>;
+  return (
+    <nav className="w-full p-4 flex items-center gap-4">
+      <HiOutlineMenu />
+      Navbar
+    </nav>
+  );
 };
 
 export default Navbar;
