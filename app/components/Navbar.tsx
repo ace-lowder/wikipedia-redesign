cffe5031f9cac8d0069ebbc9a5643f992a491d3b
Ace <ace.lowder@gmail.com>
2024-09-05 18:33:03 -0700

    Add icon to search bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { HiOutlineMenu } from "react-icons/hi";
 import { FaRegUser } from "react-icons/fa6";
 import { MdOutlinePalette } from "react-icons/md";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
@@ -13,10 +14,9 @@ const Navbar = () => {
           </button>
           <Image src="/logo.png" width={198} height={48} alt="logo" />
         </div>
-        <input
-          className="absolute top-20 left-[20%] w-1/2 md:static md:grow mx-8 max-w-[800px] bg-gray-100 px-4 rounded-lg h-9 cursor-pointer hover:bg-gray-200"
-          placeholder="Search Wikipedia"
-        />
+        <div className="absolute top-20 left-[20%] w-1/2 flex justify-between text-gray-400 items-center md:static md:grow mx-8 max-w-[800px] bg-gray-100 px-4 rounded-lg h-9 cursor-pointer hover:bg-gray-200">
+          Search Wikipedia <IoSearchSharp />
+        </div>
         <div id="options" className="flex gap-2 items-center">
           <button className="bg-yellow-400 hover:bg-yellow-300 text-gray-700 h-[34px] px-3 text-sm font-semibold rounded-md text-nowrap">
             Go PRO
